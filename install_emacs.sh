93a460d6957d6518d886a0c7bb22bac6d6bdea2e
Joe Foran <joseph-foran@highreso.co.jp>
2026-05-19 14:47:08 +0900

    Add emacs install script

---
+++ install_emacs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DOTFILE_DIR=$(realpath $(dirname $0))
+
+run_cmd () {
+    echo "========================================"
+    echo $1
+    echo "----------------------------------------"
+    eval $1
+    echo "========================================"
+}
+
+run_cmd "mamba create -n tools emacs=30.2"
+run_cmd "cp -al ${DOTFILE_DIR}/dotemacs ${HOME}/.emacs.d"
+run_cmd "cat ${DOTFILE_DIR}/emacs_alias >> ~/.bash_aliases"
